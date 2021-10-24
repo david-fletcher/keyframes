61baa5025202a1cc31a0b7a1d2c47e597ddf62e3
David Fletcher <=>
2021-10-23 21:23:25 -0400

    first draft at readme and code

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# keyframes
-An Aseprite extension that allows you to define and jump between keyframes!
+# Keyframes
+![hero image](./assets/hero.png)
+
+An Aseprite extension that allows you to add and jump between keyframes in the timeline!
+
+## How to Use Keyframes
+
+1. Download this extension by visiting the releases page! (Double click the extension once downloaded to install)
+2. In Aseprite, you can find the new menu options under the `Frames` menu.
+3. Check out the [Accessing Keyframes Controls](#accessing-keyframes-controls) below to see how you can access the Keyframes functionality!
+
+![demonstration gif](https://media.giphy.com/media/vrBmZKdiZeYyyjoIkd/giphy-downsized-large.gif)
+
+## What is a Keyframe?
+
+A Keyframe is an idea taken from other programs that also have timelines. While keyframes also have a formal definition in animation, for the purpose of this plugin, a Keyframe is simply a saved frame that you'd like to access quickly. That way, instead of trying to remember which frames you deem important or special, you can save them with plugin, then use the `Go To Previous Keyframe` or `Go To Next Keyframe` to jump between them.
+
+Keyframes are stored on a special layer created for you when you add your first keyframe. It is titled "KEYFRAMES". The script will lock this layer so that you don't accidentally draw on it. As you can see in the image below, a Keyframe is a frame on the KEYFRAMES layer that has the cel filled. A normal frame is one with an empty cel. This is your visual indicator of where a keyframe has been placed.
+
+![keyframe example](./assets/keyframe-example.png)
+
+## Accessing Keyframes Controls
+1. From the menu (the simplest method):
+    * Click the `Frames` menu
+    * Choose an option from the dropdown (add, delete, previous, or next)
+2. From a dialog window:
+    * Click the `Frames` menu
+    * Select `Open Keyframe Editor`
+    * Use the pop-up to add, delete, or jump between keyframes
+3. From keyboard shortcuts:
+    * Open the shortcut editor (`Edit > Keyboard Shortcuts`)
+    * Use the search bar at the top left and type `Keyframe`
+    * Add shortcuts as needed to each menu item
+
+## Credits
+
+This extension was commissioned by [@qamaart](https://twitter.com/qamaart) on Twitter. He also created the banner image at the top of this page! He's an incredible artist and I highly recommend you follow his work.
+
+As an advocate of open-source software, feel free to suggest edits, or just fork this repository and make your own! The license on this software is open for commercial and private use. This extension will remain free forever; however, if you'd like to buy me a coffee, you can do so here: 
+
+[![ko-fi](https://ko-fi.com/img/githubbutton_sm.svg)](https://ko-fi.com/L3L766S5F)
---
+++ extension/plugin.lua
@@ -0,0 +1,269 @@
+-- MIT License
+
+-- Copyright (c) 2021 David Fletcher
+
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
+function init(plugin)
+    print("Aseprite is initializing Keyframes")
+
+    -----------------------------------
+    -- CREATE ERROR DIALOG
+    -----------------------------------
+    -- param: str - a string value that will be displayed to the user (usually an error message)
+    -- param: dialog - the Dialog object (if any) that spawned this error message
+    -- param: exit - a boolean value; 0 to keep dialog open, and 1 to close dialog
+    -----------------------------------
+    -- This function creates a simple dialog window that will alert the user of an error that occured.
+    -- If you call this from a Dialog function, you can optionally close the dialog to end user interaction
+    -- and prevent further errors (making the user try again).
+    -----------------------------------
+    local function create_error(str, dialog, exit)
+        app.alert(str)
+        if (exit == 1) then dialog:close() end
+    end
+
+    -----------------------------------
+    -- CREATE CONFIRM DIALOG
+    -----------------------------------
+    -- param: str - a string value that will be displayed to the user (usually a question if they'd like to confirm an action)
+    -- returns: true, if the user clicked "Confirm"; nil, if the user clicked "Cancel" or closed the dialog
+    -----------------------------------
+    -- This function creates a simple dialog window that will display a message, along with two buttons: "Cancel" and "Confirm".
+    -- You can observe whether or not the user closed the dialog in this way:
+    -- local confirmed = create_confirm("Would you like to continue?")
+    -- if (not confirmed) then
+    -- end
+    -----------------------------------
+    local function create_confirm(str)
+        local confirm = Dialog("Confirm?")
+
+        confirm:label {
+            id="text",
+            text=str
+        }
+
+        confirm:button {
+            id="cancel",
+            text="Cancel",
+            onclick=function()
+                confirm:close()
+            end
+        }
+
+        confirm:button {
+            id="confirm",
+            text="Confirm",
+            focus=true,
+            onclick=function()
+                confirm:close()
+            end
+        }
+
+        -- show to grab centered coordinates
+        confirm:show{ wait=true }
+
+        return confirm.data.confirm
+    end
+
+
+    -----------------------------------
+    -- MAIN LOGIC
+    -----------------------------------
+    local function getKeyframeLayer()
+        for _,layer in ipairs(app.activeSprite.layers) do
+            if (layer.name == "KEYFRAMES") then
+                return layer
+            end
+        end
+    
+        local confirm = create_confirm("The KEYFRAMES layer could not be found. Would you like to have it added to your project?")
+        if (confirm) then
+            local layer = app.activeSprite:newLayer()
+            layer.name = "KEYFRAMES"
+            -- lock the frame so that the user has trouble editing it
+            layer.isEditable = false
+            return layer
+        else
+            return nil
+        end
+    end
+    
+    local function isCelFilled(layer, framenum)
+        return layer:cel(framenum)
+    end
+    
+    local function addKeyframe()
+        local klayer = getKeyframeLayer()
+        if (klayer == nil) then create_error("I could not add a keyframe because there is no KEYFRAMES layer.", nil, 0) return  end
+
+        -- lock the frame so that the user has trouble editing it
+        klayer.isEditable = false
+        app.activeSprite:newCel(klayer, app.activeFrame)
+        app.refresh()
+    end
+    
+    local function deleteKeyframe()
+        local klayer = getKeyframeLayer()
+        if (klayer == nil) then create_error("I could not delete a keyframe because there is no KEYFRAMES layer.", nil, 0) return  end
+
+        -- lock the frame so that the user has trouble editing it
+        klayer.isEditable = false
+        if (isCelFilled(klayer, app.activeFrame.frameNumber)) then
+            app.activeSprite:deleteCel(klayer, app.activeFrame)
+        end
+        app.refresh()
+    end
+    
+    local function gotoPrevious()
+        local klayer = getKeyframeLayer()
+        if (klayer == nil) then create_error("I could not go to the previous keyframe because there is no KEYFRAMES layer.", nil, 0) return  end
+
+        -- lock the frame so that the user has trouble editing it
+        klayer.isEditable = false
+        for fnum=app.activeFrame.frameNumber-1,1,-1 do
+            if (isCelFilled(klayer, fnum)) then
+                app.activeFrame = app.activeSprite.frames[fnum]
+                app.refresh()
+                return
+            end
+        end
+    end
+    
+    local function gotoNext()
+        local klayer = getKeyframeLayer()
+        if (klayer == nil) then create_error("I could not go to the next keyframe because there is no KEYFRAMES layer.", nil, 0) return  end
+
+        -- lock the frame so that the user has trouble editing it
+        klayer.isEditable = false
+        for fnum=app.activeFrame.frameNumber+1,#app.activeSprite.frames do
+            if (isCelFilled(klayer, fnum)) then
+                app.activeFrame = app.activeSprite.frames[fnum]
+                app.refresh()
+                return
+            end
+        end
+    end
+
+    -----------------------------------
+    -- HELPER DIALOG (OPTIONAL)
+    -----------------------------------
+    local function keyframeEditor()
+        local dialog = Dialog("Keyframes")
+
+        dialog:button {
+            id="add",
+            text="Add Keyframe",
+            onclick=function()
+                addKeyframe()
+            end
+        }
+
+        dialog:button {
+            id="delete",
+            text="Delete Keyframe",
+            onclick=function()
+                deleteKeyframe()
+            end
+        }
+
+        dialog:newrow()
+
+        dialog:button {
+            id="previous",
+            text="Previous Keyframe",
+            onclick=function()
+                gotoPrevious()
+            end
+        }
+
+        dialog:button {
+            id="next",
+            text="Next Keyframe",
+            onclick=function()
+                gotoNext()
+            end
+        }
+
+        return dialog
+    end
+
+    -----------------------------------
+    -- PLUGIN COMMANDS
+    -----------------------------------
+    -- A plugin command is simply a menu element that, when clicked, runs the function passed to onclick.
+    -- In this file, we specify menu element definitions, but abstract away their implementations to different Lua scripts.
+    -- This is done for organizational purposes, and to ease the iterative development of scripts. With this structure,
+    -- we can develop the script file independently of the plugin definition, using Aseprite's scripting engine
+    -- to run scripts as we build out our functionality, then wrap the finalized scripts in a plugin.
+    -----------------------------------
+    -- We use the parameter to init, plugin, to access the preferences table. This table will be saved on exit and restored
+    -- upon re-entry, so that you can easily save any user-defined preferences you need to. You will notice that we use
+    -- loadfile() to create a Lua chunk based on our script, and then pass the preferences table to the script as an argument.
+    -- The script can modify that table, which will then be saved automagically by Aseprite's API.
+    -----------------------------------
+    plugin:newCommand {
+        id="open_editor",
+        title="Open Keyframe Editor",
+        group="cel_frames",
+        onclick=function()
+            keyframeEditor():show{ wait=false }
+        end
+    }
+
+    plugin:newCommand {
+        id="add_keyframe",
+        title="Add Keyframe",
+        group="cel_frames",
+        onclick=function()
+            addKeyframe()
+        end
+    }
+
+    plugin:newCommand {
+        id="delete_keyframe",
+        title="Delete Keyframe",
+        group="cel_frames",
+        onclick=function()
+            deleteKeyframe()
+        end
+    }
+
+    plugin:newCommand {
+        id="prev_keyframe",
+        title="Go To Previous Keyframe",
+        group="cel_frames",
+        onclick=function()
+            gotoPrevious()
+        end
+    }
+
+    plugin:newCommand {
+        id="next_keyframe",
+        title="Go To Next Keyframe",
+        group="cel_frames",
+        onclick=function()
+            gotoNext()
+        end
+    }
+end
+  
+function exit(plugin)
+    print("Aseprite is closing Keyframes")
+end
